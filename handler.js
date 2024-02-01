b637c84bf447f9eca6161ec75cd2f6829393c3fd
DzzzD <mi20098@alas.matf.bg.ac.rs>
2024-02-01 16:15:45 +0100

    Feature: Rest of the CRUD functions added Works

--- handler.js
+++ handler.js
@@ -2,8 +2,34 @@
 
 module.exports.createNote = async (event) => {
   return {
-    statusCode: 200,
+    statusCode: 201,
     body: JSON.stringify("New note created"),
+  };
+
+}
+
+module.exports.updateNote = async (event) => {
+  let notesId = event.pathParameters.id //iz URL cita id...
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Updated note " + notesId),
+  };
+
+}
+
+module.exports.deleteNote = async (event) => {
+  let notesId = event.pathParameters.id
+  return {
+    statusCode: 200,
+    body: JSON.stringify("Deleted note " + notesId),
+  };
+
+}
+
+module.exports.getAllNotes = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify("All notes fetched"),
   };
 
 }
