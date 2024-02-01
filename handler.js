3564784907dcc245958d18af62bcca0f37fe9498
DzzzD <mi20098@alas.matf.bg.ac.rs>
2024-02-01 17:57:40 +0100

    Feature: Update notes
    Removal of some hard code as well

--- handler.js
+++ handler.js
@@ -1,13 +1,20 @@
 'use strict';
 const DynamoDB = require("aws-sdk/clients/dynamodb")
 const documentClient = new DynamoDB.DocumentClient({region: 'us-east-1'})
+const NOTES_TABLE_NAME = process.env.NOTES_TABLE_NAME
 
+const send = (statusCode, data) => {
+  return {
+    statusCode,
+    body: JSON.stringify(data)
+  }
+}
 
 module.exports.createNote = async (event, context, callback) => {
   let data = JSON.parse(event.body); //ono sto prosledjujemo POST zahtevu
   try{
     const params = {
-      TableName: "notes",
+      TableName: NOTES_TABLE_NAME,
       Item: {
         notesId: data.id,
         title: data.title,
@@ -16,26 +23,39 @@ module.exports.createNote = async (event, context, callback) => {
       ConditionExpression: "attribute_not_exists(notesId)" //ako nema id-a da throw-uje
     }
     await documentClient.put(params).promise();
-    callback(null, {
-      statusCode: 201,
-      body: JSON.stringify(data)
-    })
+    callback(null, send(201, data))
   } catch(err) {
-    callback(null, {
-      statuscode: 500,
-      body: JSON.stringify(err.message)
-    })
+    callback(null, send(500, JSON.stringify(err.message)))
   }
   
-
 }
 
-module.exports.updateNote = async (event) => {
+module.exports.updateNote = async (event, context, callback) => {
   let notesId = event.pathParameters.id //iz URL cita id...
-  return {
-    statusCode: 200,
-    body: JSON.stringify("Updated note " + notesId),
-  };
+  let data = JSON.parse(event.body)
+  try{
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+      Key: {notesId},
+      UpdateExpression: 'set #title= :title, #body = :body', //kul rstva
+      ExpressionAttributeNames: {
+        '#title': 'title',
+        '#body': 'body'
+      },
+      ExpressionAttributeValues: {
+        ':title': data.title,
+        ':body': data.body
+      },
+      ConditionExpression: 'attribute_exists(notesId)'
+    }
+
+    await documentClient.update(params).promise()
+    callback(null, send(200, data))
+
+  } catch(err) {
+    callback(null, send(500, err.message))
+  }
+
 
 }
 
